835e9c9c747192421f2d1c6ad6c07b842d4cb722
Daniil Tsaryov <daniil.tsaryov@gmail.com>
2018-06-19 14:29:18 +0400

    Unable to edit Booking on calendar view #13

--- modules/global/src/com/company/samplebooking/service/BookingDTO.java
+++ modules/global/src/com/company/samplebooking/service/BookingDTO.java
@@ -1,28 +1,34 @@
 package com.company.samplebooking.service;
 
 import com.company.samplebooking.entity.Booking;
+import com.company.samplebooking.entity.Resource;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 public class BookingDTO implements Serializable {
 
     private static final long serialVersionUID = -2959685541815794230L;
 
-    final String title;
-    final Date start;
-    final Date end;
-    final String owner;
-    final ResourceDTO resource;
+    private final UUID id;
+    private final String title;
+    private final Date start;
+    private final Date end;
+    private final String owner;
+    private final ResourceDTO resource;
 
     BookingDTO(Booking booking) {
+        this.id = booking.getId();
         this.title = booking.getTitle();
         this.start = booking.getStart();
         this.end = booking.getEnd();
         this.owner = booking.getOwner().getName();
-        this.resource = new ResourceDTO(
-                booking.getResource().getName(),
-                booking.getResource().getCategory().getName());
+        this.resource = new ResourceDTO(booking.getResource());
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -43,5 +49,26 @@ public class BookingDTO implements Serializable {
 
     public ResourceDTO getResource() {
         return resource;
+    }
+
+    public static class ResourceDTO implements Serializable {
+
+        private static final long serialVersionUID = 7434504728945715345L;
+
+        final String name;
+        final String category;
+
+        ResourceDTO(Resource resource) {
+            this.name = resource.getName();
+            this.category = resource.getCategory().getName();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCategory() {
+            return category;
+        }
     }
 }
